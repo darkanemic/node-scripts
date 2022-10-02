820b8b301aae105d75a5b785ad446e231d1decc0
darkanemic <38096325+darkanemic@users.noreply.github.com>
2022-10-02 23:49:51 +0300

    Create ObolExitProcedurePart2.sh

---
+++ ObolExitProcedurePart2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd $HOME/charon-distributed-validator-node/.charon
+cp validator_keys/keystore-0.* exit_keys
+cd $HOME/charon-distributed-validator-node
+docker compose -f compose-voluntary-exit.yml up
