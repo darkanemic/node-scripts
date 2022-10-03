b44635cb3c6850ed9794c1f9330ecb5ac222c6b7
darkanemic <38096325+darkanemic@users.noreply.github.com>
2022-10-03 16:38:38 +0300

    Update ObolExitProcedurePart2.sh

--- ObolExitProcedurePart2.sh
+++ ObolExitProcedurePart2.sh
@@ -2,4 +2,4 @@
 cd $HOME/charon-distributed-validator-node/.charon
 cp validator_keys/keystore-0.* exit_keys
 cd $HOME/charon-distributed-validator-node
-docker compose -f compose-voluntary-exit.yml up
+docker-compose -f compose-voluntary-exit.yml up
