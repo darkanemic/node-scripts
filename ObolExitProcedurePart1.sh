acac075f507002dc240b48a8001b492e071c566c
darkanemic <38096325+darkanemic@users.noreply.github.com>
2022-10-04 10:29:22 +0300

    Update ObolExitProcedurePart1.sh

--- ObolExitProcedurePart1.sh
+++ ObolExitProcedurePart1.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-cd $HOME/charon-distributed-validator-node && docker-compose down
-cp docker-compose.yml docker-compose.yml_bkp && git pull
-docker-compose up -d
+docker-compose -f $HOME/charon-distributed-validator-node/docker-compose.yml down
+cd $HOME/charon-distributed-validator-node
+cp docker-compose.yml docker-compose.yml_bkp
+git reset --hard
+git pull
+git checkout -- $HOME/charon-distributed-validator-node/docker-compose.yml
+cp $HOME/charon-distributed-validator-node/.env.sample $HOME/charon-distributed-validator-node/.env
+echo -e "\nGETH_PORT_HTTP=18545" >> $HOME/charon-distributed-validator-node/.env
+echo -e "\nLIGHTHOUSE_PORT_P2P=19000" >> $HOME/charon-distributed-validator-node/.env
+echo -e "\nMONITORING_PORT_GRAFANA=4000" >> $HOME/charon-distributed-validator-node/.env
+echo -e "\nCHARON_P2P_EXTERNAL_HOSTNAME=$(curl -s ifconfig.me)" >> $HOME/charon-distributed-validator-node/.env
+sed -i -e 's/9100:9100/19100:9100/' $HOME/charon-distributed-validator-node/docker-compose.yml
+sudo chmod -R 777 .charon
+docker-compose -f $HOME/charon-distributed-validator-node/docker-compose.yml up -d
+echo "--------------------------------------------------"
+echo "Обновление завершено"
