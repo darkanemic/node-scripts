cee63558670ce9b452ae2f88a48b91c57f2a038e
darkanemic <38096325+darkanemic@users.noreply.github.com>
2022-10-07 20:15:59 +0300

    Create take_airdrop.sh

---
+++ Solana/take_airdrop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while true
+do
+        solana airdrop 1
+        printf "sleep"
+        for((sec=0; sec<60; sec++))
+        do
+                printf "."
+                sleep 1
+        done
+        printf "\n
+done
