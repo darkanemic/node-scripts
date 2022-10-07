6c6633f0746de21a37628d2dc5490905eb13deb0
darkanemic <38096325+darkanemic@users.noreply.github.com>
2022-10-07 20:50:19 +0300

    Update take_airdrop.sh

--- Solana/take_airdrop.sh
+++ Solana/take_airdrop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-while true
+for((turn=1; turn<11; turn++))
 do
+        printf "Turn: ""$turn\n"
         solana airdrop 1
         printf "sleep"
-        for((sec=0; sec<60; sec++))
+        for((sec=0; sec<5; sec++))
         do
                 printf "."
                 sleep 1
