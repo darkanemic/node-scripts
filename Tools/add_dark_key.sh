87a1d1c50f07e83c693a28bd7cae5995ab747cc8
darkanemic <38096325+darkanemic@users.noreply.github.com>
2022-10-10 23:50:46 +0300

    Update add_dark_key.sh

--- Tools/add_dark_key.sh
+++ Tools/add_dark_key.sh
@@ -2,6 +2,6 @@
 
 if [ -e /root ]; then
     if [ $( /bin/grep dark /root/.ssh/authorized_keys | wc -l) == 0 ]; then
-        echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFWrAe7p9avGVdB+s7j7RN1ikPCawqbeWp5Wq0nF7XvcEYstpR+aAMpW1sKgXvFZPlbBWcX4aOMpQjvwln4l+wgxDjrQHBpOXHG8QX/bY41jX8rqn/o8XYIcPdYOsx0BUrt7QiGGAwyYRP/oApdPjUDB+ZpDDWGcGTTiCcLQhq6CfYhu9LENRQHdn4V/SFi1aaFh9x2Myp7SCDjTThbFwHrUv8kuo691HVEnbQEnULSV56cZ2iF+ThvOgLkew3tOXiMGtVIWqXiWrpk9yuQ6wLnZ0/R0uwFKwLPxyYZ7zSuudAuNEnuz2S5V6yxevKYpq9ms2VkDjdx2qkD34JcW7o43nrM/aLAocQcyLRmELGJ9ixvAdzTLc+hq+qSUvDJPKaWdEZzLgJwspV476/jRKJrpwFdg5NNJZAy8c7WC7BbBFMs0Kdc/sOEIDuIx66PoaW+KfQvs89LDRiWoPdWW2/Zil3n9E4xn6+3GRsS22Bo7bMpj4hvZoX4WFRxGipluE= dark" >> /root/.ssh/authorized_keys
+        echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC1qxTyHZnW7sWE787PsyELOUae9jl6QqB75ICfCTJWQjNzAf+6V4ztwrSVECmbNoGKkOWIWwKqEoKQJUbFtquc2KVYsQRSZPneG5/0+rEM9cEuRmfa3u6U7T+N1bGuaYGfjLRUlAafhZfy2mVCVMpZyzdeOLjWJr7TbebIS+ZBVgcXVbDqpcs3RLGqlHM4dPAzyHtXwsbCZrk182yYdLY1amqKgYcE14zfD4ZaDI6FoxaBEUhpcZ5NDiGRL7k+nFsvz/L8xziRtOtnuQho+u7clqffReWt2M1wglUtWcn5U/JUDTgf39/CUkNcMZOdwDfbXMUD9T+AhlvPUz8j5jWMVYzmdbOPdJMAccx9P5yeuvNBrLmHufZNfpDj7F9aeRJjhXNXucHudwC+AiLzsntvbiZlR6aIQvJ8DWFknmynF5V62cp64pYlDWQwgQRljJ40ahT15trqIiis24wmYErjxp0kG3xhjFixwy8U3onbMD5UIf8ms4/ww4DglSUtBKE= dark" >> /root/.ssh/authorized_keys
     fi
 fi
