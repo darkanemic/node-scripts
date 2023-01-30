38747caf2c0d992fcac8c32329e894adc3ae6b3d
darkanemic <qmail4444@gmail.com>
2023-01-30 14:32:17 +0300

    cli installer update

--- Sui/install_sui_cli.sh
+++ Sui/install_sui_cli.sh
@@ -3,17 +3,17 @@ sudo apt-get update
 sudo apt-get install -y --no-install-recommends
 apt install tzdata
 apt install ca-certificates
-apt install build-essential (y)
+apt install build-essential -y
 apt install libssl-dev
-apt install libclang-dev (y)
+apt install libclang-dev -y
 apt install pkg-config
 apt install openssl
-apt install protobuf-compiler (y)
-apt install cmake (y)
-sudo apt install curl (y)
-sudo apt-get install git-all (y)
+apt install protobuf-compiler -y
+apt install cmake -y
+sudo apt install curl -y
+sudo apt-get install git-all -y
 sudo apt-get install screen
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  (жмем 1 и энтер)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 rustup install stable
 rustup update stable
