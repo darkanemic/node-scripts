b5a6746da3a855514c5cc537c7e4557669d89294
darkanemic <qmail4444@gmail.com>
2023-03-04 16:51:57 +0300

    small fix

--- Massa/init_address.sh
+++ Massa/init_address.sh
@@ -7,9 +7,11 @@ cp $HOME/massa/massa-node/config/node_privkey.key $HOME/backup_massa/node_privke
 cp $HOME/massa/massa-client/wallet.dat $HOME/backup_massa/wallet.dat
 sleep 2s
 massa_wallet_address=$(./massa-client --pwd $massa_pass wallet_info | grep Address | awk '{ print $2 }')
+sleep 2s
 echo "Ваш адресс для запроса токенов:" $massa_wallet_address
+sleep 2s
 echo "На вашем кошельке уже должны быть токены запрошеные в дискорде"
-
+sleep 2s
 balance=$(./massa-client --pwd $massa_pass wallet_info | grep "Balance" | awk '{ print $2 }' | sed 's/final=//;s/,//')
 int_balance=${balance%%.*}
 echo "balance" $int_balance
@@ -20,12 +22,17 @@ if [ $int_balance -gt "100" ]; then
     echo $resp
     wallet_info=$(./massa-client --pwd $massa_pass wallet_info)
     echo $wallet_info
+    sleep 2s
 elif [ $int_balance -gl "100" ]; then
     echo "На балансе менее 100 монет. Запросите дополнительные монеты в дискорде Massa"
+    sleep 2s
 fi
 echo "Регистрируем стейкинг адресс:"
+sleep 2s
 echo node_start_staking $massa_wallet_address
-echo "Ваш адресс стейкинга теперь:" $(node_get_staking_addresses)
+sleep 2s
+echo "Ваш адресс стейкинга теперь:" $(./massa-client --pwd $massa_pass node_get_staking_addresses)
+sleep 2s
 echo "Введите ваш discordID(Узнать можно в боте Massa):"
 read massa_discord_id
 node_testnet_rewards_program_ownership_proof $massa_wallet_address $massa_discord_id
