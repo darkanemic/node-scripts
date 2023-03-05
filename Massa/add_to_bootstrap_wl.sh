f1d2c6676a1833ff22f798fe61a69f5b82fc8144
darkanemic <qmail4444@gmail.com>
2023-03-05 21:13:59 +0300

    1

--- Massa/add_to_bootstrap_wl.sh
+++ Massa/add_to_bootstrap_wl.sh
@@ -1,31 +1,41 @@
-#!/bin/bash
-
-function OutputPause {
-	sleep 2s
-}
-
-function colors {
-  GREEN="\e[32m"
-  RED="\e[39m"
-  NORMAL="\e[0m"
-}
-
-function line {
-  echo -e "${GREEN}-----------------------------------------------------------------------------${NORMAL}"
-}
-
-function AddToWL {
-	read -r -p "Введите адрес ноды которую будем прикуривать:" NeedBootstrapIP
-	echo $(${CLI} node_bootstrap_whitelist add $NeedBootstrapIP)
-	line
-	echo "IP $NeedBootstrapIP добавлен в whaitelist"
-	line
-}
-
-clear
-colors
-cd $HOME/massa/massa-client/
-source $HOME/.profile
-CLI="$HOME/massa/massa-client/./massa-client --pwd ${massa_pass}"
-
-AddToWL
+ #!/bin/bash
+ 2
+ 3
+ 4 function OutputPause {
+ 5         sleep 2s
+ 6 }
+ 7
+ 8
+ 9 function colors {
+10   GREEN="\e[32m"
+11   RED="\e[39m"
+12   NORMAL="\e[0m"
+13 }
+14
+15
+16 function line {
+17   echo -e "${GREEN}-----------------------------------------------------------------------------${NORMAL}"
+18 }
+19
+20
+21 function show_whitelist {
+22         line
+23         cat $HOME/massa/massa-node/base_config/bootstrap_whitelist.json
+24         line
+25 }
+26
+27
+28 function add_to_whitelist {
+29         read -r -p "Введите адрес ноды которую будем прикуривать:" NeedBootstrapIP
+30         echo $(${CLI} node_bootstrap_whitelist add $NeedBootstrapIP)
+31         line
+32         echo "IP $NeedBootstrapIP добавлен в whitelist"
+33         show_whitelist
+34 }
+35
+36 CLI="$HOME/massa/massa-client/./massa-client --pwd ${massa_pass}"
+37
+38 clear
+39 colors
+40 cd $HOME/massa/massa-client/
+41 add_to_whitelist
