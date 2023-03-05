2710f565fa2018ec8fa945d317f14830345bb01d
darkanemic <qmail4444@gmail.com>
2023-03-06 00:36:48 +0300

    1

--- Massa/buy_rolls_until_have_money.sh
+++ Massa/buy_rolls_until_have_money.sh
@@ -41,8 +41,9 @@ function get_wallet_info {
 }
 
 
-function wait(sec) {
+function wait() {
     printf "sleep"
+        sec=$1
         for((m=0; m<$sec; m++))
         do
                 printf "."
