df51a3b4b97a737a6ecd63d77d146153a29a89b0
darkanemic <qmail4444@gmail.com>
2023-03-05 18:05:23 +0300

    Renew

---
+++ Massa/RegMassaAccount.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+function OutputPause(){
+	sleep 2s
+}
+
+function BackupKeys(){
+	echo "Делаем бэкап ключей в папку $HOME/backup_massa/"
+	mkdir -p $HOME/backup_massa
+	cp $HOME/massa/massa-node/config/node_privkey.key $HOME/backup_massa/node_privkey.key
+	cp $HOME/massa/massa-client/wallet.dat $HOME/backup_massa/wallet.dat
+	OutputPause
+}
+
+function GetWalletAddress(){
+	echo $(${CLI} wallet_info | grep Address | awk '{ print $2 }')
+}
+
+function GetBalance(){
+	echo $(${CLI} wallet_info | grep "Balance" | awk '{ print $2 }' | sed 's/final=//;s/,//')
+}
+
+function CheckKeys(){
+	if [ -f $HOME/massa/massa-node/config/node_privkey.key ]; then
+		echo true
+	else
+		echo false
+	fi
+}
+
+function GenerateKeys(){
+	if [ ! CheckKeys ]; then
+		echo "Ключи не обнаружены генерируем новые ключи..."
+		$({CLI} wallet_generate_secret_key)
+		OutputPause
+	else
+		echo "Обнаружены старые ключи, будем использовать их..."
+		OutputPause
+	fi
+}
+
+function BuyRoll(){
+	echo "Покупаем Roll..."
+#	BuyRollResult=$(${CLI} buy_rolls $wallet_address 1 0)
+#	echo $BuyRollResult
+	OutputPause
+}
+
+function RegStakingAddress(){
+	echo "Регистрируем стейкинг адресс..."
+	echo $(${CLI} node_start_staking $wallet_address)
+	OutputPause
+	staking_address=$(${CLI} node_get_staking_addresses)
+	echo "Ваш адресс стейкинга теперь: $staking_address"
+	OutputPause
+}
+
+function WaitPressY(){
+	read -n 1 input
+	if [[ "$input" == "Y" || "$input" == "y" ]]; then
+		echo "Продолжаем выполнение..."
+	else
+		echo "Прерываем выполнение скрипта..."
+		exit
+	fi
+}
+
+functions SendYourIPtoBot(){
+	echo "Сообщите дискорд-боту Massa  IP вашего сервера:"
+	ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
+	echo "Я отправил IP боту"
+	WaitPressY
+}
+
+function SetupOwner(){
+	read -p "Введите ваш discordID (Узнать можно в боте Massa):" massa_discord_id
+	node_testnet_rewards_program_ownership_proof $wallet_address $massa_discord_id
+	echo "Для завершения регистрации владельца, отправте данный код боту..."
+}
+
+functino Main(){
+	CLI="$HOME/massa/massa-client/./massa-client --pwd ${massa_pass}"
+	wallet_address=$(GetWalletAddress)
+	balance=$(GetBalance)
+
+	GenerateKeys
+	BackupKeys
+	SendYourIPtoBot
+
+	integer_balance=${balance%%.*}
+
+	if [ $integer_balance -gt "100" ]; then
+		echo "На балансе есть $integer_balance токенов..."
+		BuyRoll
+		OutputPause
+	else
+		echo "На балансе менее 100 монет. Запросите дополнительные монеты в дискорде Massa в ветке #testnet-faucet"
+		echo "Я уже запросили токены, подождал пару минут и мы можем продолжать (Y). Прервать скрипт (Нажатие любой другой клавиши)"
+		WaitPressY
+		echo "На балансе есть $integer_balance токенов..."
+		OutputPause
+		BuyRoll
+	fi
+	RegStakingAddress
+}
+
+clear
+cd $HOME/massa/massa-client/
+source $HOME/.profile
+
+Main
