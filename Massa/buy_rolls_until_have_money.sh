2c93e33bd66979cd41a773697ef8d18d0e964df9
darkanemic <qmail4444@gmail.com>
2023-03-06 00:05:02 +0300

    1

--- Massa/buy_rolls_until_have_money.sh
+++ Massa/buy_rolls_until_have_money.sh
@@ -65,7 +65,7 @@ do
         date=$(date +"%H:%M")
         echo Last Update: ${date}
         printf "sleep..."
-        for((m=0; m<10; m++))
+        for((m=0; m<60; m++))
         do
                 printf "."
                 sleep 1s
