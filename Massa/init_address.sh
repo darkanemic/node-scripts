01cc3c9ad164a80a64d061684bc0ed3888a0cbdb
darkanemic <qmail4444@gmail.com>
2023-03-05 03:56:48 +0300

    renew

--- Massa/init_address.sh
+++ Massa/init_address.sh
@@ -1,55 +1,94 @@
 #!/bin/bash
+
+function OutputPause(){
+	sleep 2s
+}
+
 function BackupKeys(){
-    echo "Бэкапим ключи в папку $HOME/backup_massa/"
-    mkdir $HOME/backup_massa > /dev/null
-    cp $HOME/massa/massa-node/config/node_privkey.key $HOME/backup_massa/node_privkey.key
-    cp $HOME/massa/massa-client/wallet.dat $HOME/backup_massa/wallet.dat
-    sleep 3s
-    {BackupKeys}=true
+	echo "Делаем бэкап ключей в папку $HOME/backup_massa/"
+	mkdir -p $HOME/backup_massa
+	cp $HOME/massa/massa-node/config/node_privkey.key $HOME/backup_massa/node_privkey.key
+	cp $HOME/massa/massa-client/wallet.dat $HOME/backup_massa/wallet.dat
+	OutputPause
 }
 
-function GetWalletAdress(){
-    CLI=./massa-client --pwd $massa_pass
-    {GetWalletAdress}=echo $(${CLI} wallet_info | grep Address | awk '{ print $2 }')
+function GetWalletAddress(){
+	echo $(${CLI} wallet_info | grep Address | awk '{ print $2 }')
 }
 
-CLI=./massa-client --pwd $massa_pass
-massa_wallet_address=$(GetWalletAdress)
+function GetBalance(){
+	echo $(${CLI} wallet_info | grep "Balance" | awk '{ print $2 }' | sed 's/final=//;s/,//')
+}
 
-cd $HOME/massa/massa-client
-echo source $HOME/.profile
-#$({CLI} wallet_generate_secret_key)
+function CheckKeys(){
+	if [ -f $HOME/massa/massa-node/config/node_privkey.key ]; then
+		echo true
+	else
+		echo false
+	fi
+}
+
+function GenerateKeys(){
+	if [ ! CheckKeys ]; then
+		echo "Ключи не обнаружены генерируем новые ключи..."
+		$({CLI} wallet_generate_secret_key)
+		OutputPause
+	else
+		echo "Обнаружены старые ключи, будем использовать их..."
+		OutputPause
+	fi
+}
+
+function BuyRoll(){
+	echo "Покупаем Roll..."
+#	BuyRollResult=$(${CLI} buy_rolls $wallet_address 1 0)
+#	echo $BuyRollResult
+	OutputPause
+}
+
+function StakingAddressReg(){
+	echo "Регистрируем стейкинг адресс..."
+	echo $(${CLI} node_start_staking $wallet_address)
+	OutputPause
+	staking_address=$(${CLI} node_get_staking_addresses)
+	echo "Ваш адресс стейкинга теперь: $staking_address"
+	OutputPause
+}
+
 clear
+source $HOME/.profile
+CLI="$HOME/massa/massa-client/./massa-client --pwd ${massa_pass}"
+
+wallet_address=$(GetWalletAddress)
+GenerateKeys
 BackupKeys
-echo "Ваш адресс для запроса токенов:" $massa_wallet_address
-sleep 3s
-echo "На вашем кошельке уже должны быть токены запрошеные в дискорде"
-sleep 3s
-balance=$(${CLI} wallet_info | grep "Balance" | awk '{ print $2 }' | sed 's/final=//;s/,//')
-int_balance=${balance%%.*}
-echo "balance" $int_balance
-if [ $int_balance -gt "100" ]; then
-    echo "На балансе есть " $int_balance "токенов. Приступаем к покупке первого ролла..."
-    sleep 2s
-    resp=$(./massa-client --pwd $massa_pass buy_rolls $massa_wallet_address 1 0)
-    echo $resp
-    wallet_info=$(./massa-client --pwd $massa_pass wallet_info)
-    echo $wallet_info
-    sleep 2s
-elif [ $int_balance -gl "100" ]; then
-    echo "На балансе менее 100 монет. Запросите дополнительные монеты в дискорде Massa"
-    sleep 2s
+
+echo "Ваш адресс для запроса токенов: $wallet_address"
+OutputPause
+
+balance=$(GetBalance)
+integer_balance=${balance%%.*}
+
+if [ $integer_balance -gt "100" ]; then
+	echo "На балансе есть $integer_balance токенов..."
+	BuyRoll
+	OutputPause
+else
+	echo "На балансе менее 100 монет. Запросите дополнительные монеты в дискорде Massa в ветке #testnet-faucet"
+	echo "Я уже запросили токены, подождал пару минут и мы можем продолжать (Y). Прервать скрипт (Нажатие любой другой клавиши)"
+	read -n 1 input
+	if [[ "$input" == "Y" || "$input" == "y" ]]; then
+		echo "Продолжаем выполнение..."
+		OutputPause
+		echo "На балансе есть $integer_balance токенов..."
+		OutputPause
+		BuyRoll
+	else
+		echo "Прерываем выполнение скрипта..."
+		exit
+	fi
 fi
-echo "Регистрируем стейкинг адресс:"
-sleep 2s
-echo node_start_staking $massa_wallet_address
-sleep 2s
-massa_staking_address=$(./massa-client --pwd $massa_pass node_get_staking_addresses)
-echo "Ваш адресс стейкинга теперь:" $massa_staking_address
-sleep 2s
-echo "Введите ваш discordID(Узнать можно в боте Massa):"
-read massa_discord_id
-node_testnet_rewards_program_ownership_proof $massa_wallet_address $massa_discord_id
+StackingAddressReg
+read -p "Введите ваш discordID (Узнать можно в боте Massa):" massa_discord_id
+node_testnet_rewards_program_ownership_proof $wallet_address $massa_discord_id
 echo "Для завершения регистрации, отправте данный код боту..."
-date=$(date +"%H:%M")
-echo Update : ${date}
