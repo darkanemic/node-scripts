0754d4960ed95c86b9c4a1a94a52c49ea2bb2359
darkanemic <qmail4444@gmail.com>
2023-03-09 19:14:45 +0300

    1

--- Obol/reinstall_with_keys_backup.sh
+++ Obol/reinstall_with_keys_backup.sh
@@ -57,7 +57,7 @@ function remove_obol {
 function clear_volumes {
     echo -e "${GOOD} Clearing containers/images/volumes... $HOME/backup_obol ${NORMAL}"
     line
-    echo "y" | docker container prune && echo "y" | docker image prune -a && echo "y" | docker volume prune -f
+    echo "y" | docker container prune &> /dev/null && echo "y" | docker image prune -a &> /dev/null && echo "y" | docker volume prune -f &> /dev/null
 }
 
 function install_obol {
