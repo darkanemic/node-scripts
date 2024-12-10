e25925a72428a2480926a5c0ac0500fd3fc02f9b
darkanemic <38096325+darkanemic@users.noreply.github.com>
2024-12-10 14:56:01 +0300

    Update monitoring_solana.sh

--- solana/monitoring_solana.sh
+++ solana/monitoring_solana.sh
@@ -1,5 +1,35 @@
+#!/bin/bash
+if [ ! $NODENAME ]; then
+	read -p "Enter node name: " NODENAME
+fi
+echo 'Your node name: ' $NODENAME
+sleep 1
+if [ ! $CHAIN ]; then
+	read -p "testnet or mainnet???: " CHAIN
+fi
+echo 'Your chain is: ' $CHAIN
+sleep 1
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D8FF8E1F7DF8B07E
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/influxdata.list
+deb https://repos.influxdata.com/ubuntu bionic stable
+EOF
+
+sudo apt-get -y install gnupg1 gnupg2
+
+sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install telegraf jq bc git
+sudo adduser telegraf sudo
+sudo adduser telegraf adm
+sudo -- bash -c 'echo "telegraf ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
+sudo -- bash -c 'echo "solana ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
+sudo cp /etc/telegraf/telegraf.conf /etc/telegraf/telegraf.conf.orig
+sudo rm -rf /etc/telegraf/telegraf.conf
+git clone https://github.com/stakeconomy/solanamonitoring/
 cd solanamonitoring
-git checkout 830f7ddeca92924dc8e2c557770031c15b33553c
+#git checkout 830f7ddeca92924dc8e2c557770031c15b33553c
 chmod +x monitor.sh
 
 cat <<EOF | sudo tee /etc/telegraf/telegraf.conf
